84863d83c6975f262b332065426833fe5685f011
Ricardo Zuno <zunosan.ricardo506@gmail.com>
2022-01-08 19:02:50 -0600

    Agregando validación en crear cuenta y confirmar cuenta

--- controllers/loginController.php
+++ controllers/loginController.php
@@ -2,6 +2,8 @@
 
 namespace Controllers;
 
+use Classes\Email;
+use Model\Usuario;
 use MVC\Router;
 
 class LoginController{
@@ -24,8 +26,72 @@ class LoginController{
     }
 
     public static function crear(Router $router){
-        $router->render('auth/crear-cuenta', [
+        $usuario = new Usuario();
 
+        //Alertas vacias
+        $alertas = [];
+        if($_SERVER['REQUEST_METHOD'] === 'POST'){
+            $usuario->sincronizar($_POST);
+            $alertas = $usuario->validarNuevaCuenta();
+
+            //Revisar que alertas esté vacío
+            if (empty($alertas)) {
+                //Verificar que el usuario no está registrado
+                $resultado = $usuario->existeUsuario();
+                if ($resultado->num_rows) {
+                    $alertas = Usuario::getAlertas();
+                }else{
+                    //Hashear el password
+                    $usuario->hashPassword();
+
+                    //Generar un token único
+                    $usuario->crearToken();
+
+                    //Enviar el email
+                    $email = new Email($usuario->email, $usuario->nombre, $usuario->token);
+                    $email->enviarConfirmacion();
+
+                    //Crear el usuario
+                    $resultado = $usuario->guardar();
+                    if ($resultado) {
+                        header('Location: /mensaje');
+                    }
+                }
+            }
+        }
+
+        $router->render('auth/crear-cuenta', [
+            'usuario' => $usuario,
+            'alertas' => $alertas
+        ]);
+    }
+
+    public static function mensaje(Router $router){
+        $router->render('auth/mensaje');
+    }
+
+    public static function confirmar(Router $router){
+        $alertas = [];
+        $token = s($_GET['token']);
+        $usuario = Usuario::where('token', $token);
+
+        if (empty($usuario)) {
+            //Mostrar mensaje de error
+            Usuario::setAlerta('error', 'Token no válido');
+        }else{
+            //Modificar a usaurio confirmado
+            $usuario->confirmado = '1';
+            $usuario->token = null;
+            $usuario->guardar();
+            Usuario::setAlerta('exito', 'Cuenta confirmada correctamente');
+        }
+
+        //Obtener alertas
+        $alertas = Usuario::getAlertas();
+        
+        //Renderizar la vista
+        $router->render('auth/login', [
+            'alertas' => $alertas
         ]);
     }
 }
---
+++ views/auth/confirmar-cuenta.php
@@ -0,0 +1,9 @@
+<h1 class="nombre-pagina">Confirmar Cuenta</h1>
+
+<?php 
+    include_once __DIR__ .'/../templates/alertas.php';
+?>
+
+<p class="descripcion-pagina">Sólo debes iniciar sesión</p>
+
+<a href="/" class="boton">Iniciar Sesión</a>
