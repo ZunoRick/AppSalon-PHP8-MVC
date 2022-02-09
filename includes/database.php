8339761dc846a7223042c80a8ea1fc0b3f314b41
Ricardo Zuno <zunosan.ricardo506@gmail.com>
2022-02-08 22:49:36 -0600

    Configurado archivos para deployment

--- includes/database.php
+++ includes/database.php
@@ -5,7 +5,8 @@ $db =  mysqli_connect(
     $_ENV['DB_HOST'], 
     $_ENV['DB_USER'], 
     $_ENV['DB_PASS'], 
-    $_ENV['DB_NAME']
+    $_ENV['DB_NAME'], 
+    $_ENV['DB_PORT'] ?? 3310
 );
 $db -> set_charset('utf8');
 
