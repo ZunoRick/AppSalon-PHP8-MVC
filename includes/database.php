61b0ab77fe83e116738bd187be4c41bdfcbc441c
Ricardo Zuno <zunosan.ricardo506@gmail.com>
2022-02-08 19:13:12 -0600

    Agregando segunda config de deployment

--- includes/database.php
+++ includes/database.php
@@ -1,12 +1,18 @@
 <?php
 header('Access-Control-Allow-Origin: *');
 
+// $db =  mysqli_connect(
+//     $_ENV['DB_HOST'], 
+//     $_ENV['DB_USER'], 
+//     $_ENV['DB_PASS'], 
+//     $_ENV['DB_NAME'], 
+//     $_ENV['DB_PORT'] ?? ''
+// );
 $db =  mysqli_connect(
     $_ENV['DB_HOST'], 
     $_ENV['DB_USER'], 
     $_ENV['DB_PASS'], 
-    $_ENV['DB_NAME'], 
-    $_ENV['DB_PORT'] ?? ''
+    $_ENV['DB_NAME']
 );
 $db -> set_charset('utf8');
 
